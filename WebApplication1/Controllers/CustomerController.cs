8c11dc046d416996162779dcc40a34de86ef676f
AmirMohammadKomijani <amir.m.k8011@gmail.com>
2024-08-17 17:59:28 +0330

    <Controller> : Customer Controller is initialized

--- WebApplication1/Controllers/CustomerController.cs
+++ WebApplication1/Controllers/CustomerController.cs
@@ -1,6 +1,21 @@
-﻿namespace WebApplication1.Controllers
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Data;
+
+namespace WebApplication1.Controllers
 {
-    public class CustomerController
+    [Route("api/customers")]
+    [ApiController]
+    public class CustomerController : ControllerBase
     {
+        private readonly ApplicationDbContext _db;
+        private readonly IMapper _mapper;
+
+        public CustomerController(ApplicationDbContext db,IMapper mapper)
+        {
+            _mapper = mapper;
+            _db = db;
+        }
     }
 }
