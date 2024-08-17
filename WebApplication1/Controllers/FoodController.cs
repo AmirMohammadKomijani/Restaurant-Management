3c419c599e0f51b21d1c50094880d72626dd8f2f
AmirMohammadKomijani <amir.m.k8011@gmail.com>
2024-08-17 17:57:26 +0330

    <Controller> : Food Controller is initialized

--- WebApplication1/Controllers/FoodController.cs
+++ WebApplication1/Controllers/FoodController.cs
@@ -1,6 +1,22 @@
-﻿namespace WebApplication1.Controllers
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Data;
+
+namespace WebApplication1.Controllers
 {
-    public class FoodController
+    [Route("api/foods")]
+    [ApiController]
+    public class FoodController:ControllerBase
     {
+        private readonly ApplicationDbContext _db;
+        private readonly IMapper _mapper;
+
+        public FoodController(ApplicationDbContext db,IMapper mapper)
+        {
+            _db= db;
+            _mapper= mapper;
+        }
+
+
     }
 }
