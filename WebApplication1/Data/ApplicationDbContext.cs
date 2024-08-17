1a6542237d6a2d41aac9ddbdadad0572fc28bfb2
AmirMohammadKomijani <amir.m.k8011@gmail.com>
2024-08-17 16:50:02 +0330

    <Database> : initialize all tables

---
+++ WebApplication1/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApplication1.Model;
+
+namespace WebApplication1.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        :base(options) { }
+        
+        public DbSet<Food> foods { get; set; }
+        public DbSet<Customer> customer { get; set; }
+        public DbSet<Chef> chefs { get; set; }
+    }
+}
