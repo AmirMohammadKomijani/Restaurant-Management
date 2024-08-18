b43b43e7b7d5fb6cbc24eae59de0c4af70c83261
AmirMohammadKomijani <amir.m.k8011@gmail.com>
2024-08-18 14:51:02 +0330

    <Repository> : implementing Repository interface and class.

---
+++ WebApplication1/Repository/IRepository/IRepository.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq.Expressions;
+
+namespace WebApplication1.Repository.IRepository
+{
+        public interface IRepository<T> where T : class
+        {
+            Task<List<T>> getAll(Expression<Func<T, bool>>? filter = null);
+            Task<T> getOne(Expression<Func<T, bool>>? filter = null, bool tracked = true);
+            Task Create(T entity);
+            Task Remove(T entity);
+            Task Save();
+        }
+}
---
+++ WebApplication1/Repository/Repository.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+using WebApplication1.Data;
+
+namespace WebApplication1.Repository.IRepository
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        private readonly ApplicationDbContext _db;
+        internal DbSet<T> dbSet;
+
+        public Repository(ApplicationDbContext db)
+        {
+            _db=db;
+            this.dbSet = _db.Set<T>();
+        }
+        public async Task Create(T entity)
+        {
+            await dbSet.AddAsync(entity);
+            await Save();
+        }
+
+        public async Task<T> getOne(Expression<Func<T, bool>>? filter = null, bool tracked = true)
+        {
+            IQueryable<T> query = dbSet;
+            if(!tracked) query.AsNoTracking();
+            if(filter != null) query = query.Where(filter);
+            return await query.FirstOrDefaultAsync();
+        }
+
+        public async Task<List<T>> getAll(Expression<Func<T, bool>>? filter = null)
+        {
+            IQueryable<T> query = dbSet;
+            if (filter != null) query = query.Where(filter);
+            return await query.ToListAsync();
+        }
+
+        public async Task Remove(T entity)
+        {
+            dbSet.Remove(entity);
+            await Save();
+        }
+
+        public async Task Save()
+        {
+            await _db.SaveChangesAsync();
+        }
+    }
+}
